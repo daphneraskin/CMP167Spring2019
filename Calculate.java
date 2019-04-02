4fd515e7337e3c8e800e61c23f6ea22c2d2ea247
daphneraskin <45129911+daphneraskin@users.noreply.github.com>
2019-04-02 12:14:21 -0400

    Add files via upload

---
+++ Calculate.java
@@ -0,0 +1,17 @@
+/*
+ * Author: Daphne Raskin
+ * Date: April 2nd, 2019
+ * Description: This class has to calculate methods.
+ */
+public class Calculate {
+	public static int multiply(int a, int b) {
+		return a*b;
+	}
+	public static double multiply(double a, double b) {
+		return a*b;
+	}
+	public static double getRectangleArea(double length, double width) {
+		return length*width;
+	}
+	
+}
---
+++ CalculateTest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculateTest {
+	@BeforeClass
+	public static void greet() {
+		System.out.println("Before Everything");
+	}
+	@Before
+	public void perMethod() {
+		System.out.println("Before each test");
+	}
+	@After
+	public void afterMethod() {
+		System.out.println("After each test");
+	}
+	@Test
+	public void test1() {
+		assertEquals(1, Calculate.multiply(1, 1));
+		System.out.println("Test 1");
+	}
+	@Test
+	public void test2() {
+		assertEquals(1, Calculate.multiply(1, 1));
+		System.out.println("Test 2");
+	}
+	@Test
+	public void test3() {
+		assertEquals(2.0, Calculate.multiply(1.0, 2.0), 0.0001);
+		System.out.println("Test 3");
+	}
+
+}
